fc99afd369d43334303579cf298deaa166093c98
Tehau <tsing.tehau@gmail.com>
2022-11-10 00:17:49 +0100

    create home page

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,67 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+        <nav>
+            <ul>
+                <li class="nav-item">
+                    <img alt="Vue logo" src="./assets/logo.png" style="max-height: 30px">
+                    Home
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <HomePage/>
+    </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import HomePage from './components/HomePage.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            HomePage
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    body {
+        background: linear-gradient(to bottom, #555555, #999999);
+    }
+
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+
+    header {
+        background-color: #999999;
+        width: 1084px;
+        margin: 0 auto;
+    }
+
+    ul {
+        padding: 3px;
+        display: flex;
+    }
+
+    .nav-item {
+        display: inline-block;
+        padding: 5px 10px;
+        font-size: 22px;
+        border-right: 1px solid #bbb;
+    }
+
+    main {
+        margin: 0 auto;
+        padding: 30px;
+        background-color: white;
+        width: 1024px;
+        min-height: 300px;
+    }
 </style>
